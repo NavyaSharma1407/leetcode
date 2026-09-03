a0c31657e55e1cca2096cafd45fa75b0e7a7260c
NavyaSharma1407 <navya11748@gmail.com>
2026-09-03 11:35:09 +0530

    Time: 0 ms (100%), Space: 165.7 MB (99.48%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        set<int>s;
+        // set<int>s;
+        int minOdd = INT_MAX;
         for(int it : nums1){
-            if(it % 2 != 0) s.insert(it);
+            if(it % 2 != 0 && it < minOdd) minOdd = it;
         }
-        if(s.empty()) return true;
-        if(s.size() == nums1.size()) return true;
+        if(minOdd == INT_MAX) return true;
+        // if(s.empty()) return true;
+        // if(s.size() == nums1.size()) return true;
         for(auto it : nums1){
             if(it % 2 == 0){
-                bool found = false;
-                for(auto el : s){
-                    if(it - el >= 1){
-                        found = true;
-                        break;
-                    }
-                }
-                if(found == false) return false;
+                if(it - minOdd < 1) return false;
+                // bool found = false;
+                // for(auto el : s){
+                //     if(it - el >= 1){
+                //         found = true;
+                //         break;
+                //     }
+                // }
+                // if(found == false) return false;
             }
         }
         return true;
